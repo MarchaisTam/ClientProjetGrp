d17ffab0fd565f68868272c2d4ce4c8cff8a00a2
MarchaisTam <87361035+MarchaisTam@users.noreply.github.com>
2022-10-17 09:36:21 +0200

    xml page d'accueil

--- app/src/main/java/com/example/velotoulouse/MainActivity.kt
+++ app/src/main/java/com/example/velotoulouse/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.velotoulouse
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Création de l'intent
+        //val intent = Intent(this, MapsActivity::class.java)
+
+        // Lance le workflow de changement d'écran
+        startActivity(intent)
     }
 }
